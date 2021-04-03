0df34e3320a741302db1ce74b277dc0a08200193
Sachin Kumar <Sachin.Kumar11@gds.ey.com>
2021-04-03 18:48:29 +0530

    Initial Check-in

---
+++ CatalogService/Controllers/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using CatalogService.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly DatabaseContext databaseContext;
+        public CatalogController(DatabaseContext _databaseContext)
+        {
+            databaseContext = _databaseContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return databaseContext.Products.ToList();
+        }
+    }
+}
